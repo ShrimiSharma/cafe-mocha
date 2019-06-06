14ad711e35e83bf28135d81d0caac2b0c4a34a7f
shrimi.sharma <shrimi.sharma@gmail.com>
2019-06-06 21:28:44 +0000

    added Watir

--- napp/app/controllers/application_controller.rb
+++ napp/app/controllers/application_controller.rb
@@ -1,77 +1,46 @@
   class ApplicationController < ActionController::Base
   #require 'json'
 
-  def get_asin
+    def get_asin
 
-    render template:'get_asin'
+      render template:'get_asin'
 
-  end
+    end
 
-  def scrape_amazon
+    def scrape_amazon
 
-    if params[:q].present?
-      @asin = "#{params[:q]}" 
-
-      require 'open-uri'
-    begin
-
-      #@asin = 'B002QYW8LW'
-      @doc = Nokogiri::HTML(open("https://www.amazon.com/dp/#@asin?th=1", 'User-Agent' => 'Ruby'))#{|conf| conf.noblanks}
-#       puts @doc.inspect
-      unless @doc.nil?
-        @title = @doc.at("#titleSection").text.strip
-        @category = @doc.at("#wayfinding-breadcrumbs_container").text.strip
+      if params[:q].present?
+        @asin = "#{params[:q]}" 
         
-        prod_details = @doc.css("div#prodDetails")
-        puts"#############################"
+        require 'open-uri'
+        require 'watir'
+        
+      begin
+
+        browser = Watir::Browser.new :chrome, headless:true
+        browser.goto "https://www.amazon.com/dp/#@asin?th=1"
+        sleep 2
+      
+        @doc = Nokogiri::HTML(browser.html)
+				#puts @doc.inspect
+        
+        unless @doc.nil?
+         @title = @doc.at("#titleSection").text.strip #unless @doc.at("titleSection").nil?
+         @category = @doc.at("#wayfinding-breadcrumbs_container").text.strip #unless @doc.at("#wayfinding-breadcrumbs_container").nil?
+        
+         prod_details = @doc.css("div#prodDetails")
+         puts prod_details.inspect
+					
+					if prod_details.nil?
+						prod_details = @doc.css("div#descriptionAndDetails")
+					end
+          
+          @dimensions = prod_details.at(':contains("Dimensions"):not(:has(:contains("Dimensions")))').next_element.text.strip unless prod_details.at(':contains("Dimensions"):not(:has(:contains("Dimensions")))').nil?
+          @rank = prod_details.at(':contains("Best Sellers Rank"):not(:has(:contains("Best Sellers Rank")))').next_element.text.strip.split("(")[0] unless prod_details.at(':contains("Best Sellers Rank"):not(:has(:contains("Best Sellers Rank")))').nil?
+          
        
-        prod_details.each do |pd|
-          @dimensions = pd.at(':contains("Dimensions"):not(:has(:contains("Dimensions")))').next_element.text.strip
-          puts @dimensions.inspect
-          @rank = pd.at(':contains("Best Sellers Rank"):not(:has(:contains("Best Sellers Rank")))').next_element.text.strip.split("(")[0]
-          puts @rank.inspect
-        end
-        
-        if prod_details.nil? || prod_details.empty?
-          #do what??
-          dim = @doc.xpath('//span[contains(text(), "Dimensions")]').first
-          puts dim.inspect
-          unless dim.nil? 
-            @dimensions = dim.next_element.text.strip
-          end
-          ra = @doc.xpath('//*[contains(text(), "Best Sellers Rank")]')[0]
-          unless ra.nil?
-            @rank = ra.next_element.text#.strip#.split("(")[0]
-          end
-          #@rank = @doc.xpath('//* [contains(text(), "Best Sellers Rank")]')[0].next_element.text.strip.split("(")[0]
-        end
 
-#         dim = @doc.at(':contains("Product Dimensions"):not(:has(:contains("Product Dimensions")))')
-#         unless dim.nil?
-#           puts "Here1 dim"
-#           puts dim.inspect
-#            @dimensions = dim.next.text.strip
-#         end
-#         #@dimensions = @doc.at(':contains("Product Dimensions"):not(:has(:contains("Product Dimensions")))').next.text.strip
-#         if @dimensions.nil? || @dimensions.empty?
-#           dim = @doc.xpath('//span[contains(text(), "Product Dimensions")]').first
-#           puts "Here 2 dim"
-#           puts dim.inspect
-#           unless dim.nil? 
-#             @dimensions = dim.next_element.text.strip
-#           end
-#           #@dimensions = @doc.xpath('//span[contains(text(), "Product Dimensions")]').first.next_element.text.strip
-#         end
-
-#         #@rank = @doc.at(':contains("Best Sellers Rank"):not(:has(:contains("Best Sellers Rank")))').next.text.strip.split("(")[0]
-#         #@rank = @doc.xpath('//* [contains(text(), "Best Sellers Rank")]')[0].next_element.text.strip.split("(")[0]
-#         #@rank = @doc.search "[text()*='Best Sellers Rank']"
-#         @rank = @doc.search('Best Sellers Rank').text[/\#.*\)/]
-#         puts "here i am"
-#         puts @dimensions.inspect
-#         puts @rank.inspect
-
-        #check if product exists in the db, create a new product if it doesnt already exist.
+        #check if product exists in the db, create a new product if it doesn't already exist.
         @pr = Product.find_by_asin(@asin)
         if @pr.nil?
           @p = Product.new
@@ -91,31 +60,21 @@
         else
           redirect_to @pr, notice: 'Product already exits.'
         end
-        
-       
-
-      end
-
-
-     #puts @ranks.inspect
-      #puts @tble.inspect
-      #puts "here"
-      #puts @dimensions.inspect
-      #puts @rank.inspect
-      rescue OpenURI::HTTPError => e
-        if e.message == '404 Not Found'
-          raise ActionController::RoutingError.new('Could not find this product on Amazon. Please enter a valid ASIN.')
-  else
-    redirect_to root_path, notice: 'Please enter a valid ASIN.'
-  end
-
+          
+        end
+      
+  rescue OpenURI::HTTPError => e
+    if e.message == '404 Not Found'
+      raise ActionController::RoutingError.new('Could not find this product on Amazon. Please enter a valid ASIN.')
+    else
+      redirect_to root_path, notice: 'Please enter a valid ASIN.'
     end
 
-      #render template: 'get_asin'
-     else
-      redirect_to root_path, notice: 'Please enter a valid ASIN.' 
-     end
+  end
 
+  else
+    redirect_to root_path, notice: 'Please enter a valid ASIN.' 
   end
 
   end
+end
